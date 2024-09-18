232693d05e085267b68e5c2c4bd224d7ff1c61bf
zhussai1405 <zhussai1405@gmail.com>
2024-09-18 22:10:16 +0500

    Learn_Javascript.js

---
+++ learn_Javascript.js
@@ -0,0 +1,39 @@
+// Create two variables, firstName and lastName
+let firstName = "John";
+let lastName = "Doe";
+// Concatenate the two variables into a third variable called fullName
+let fullName = firstName + " " + lastName;
+// Log fullName to the console
+console.log("conatenate the firstName and lastName," + fullName);
+
+
+// Create a function that logs out "Hi there, Linda!" when called
+let greet = "Hi there";
+let name = "Linda!";
+
+function greetName(){
+    console.log(greet + " , " + name);
+}
+greetName()
+
+
+let myPoints = 3;
+//Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+function add3Points(){
+    myPoints +=3;
+    }
+    function removePoints(){
+        myPoints -=1;
+    }
+add3Points()
+add3Points()
+add3Points()
+removePoints()
+removePoints()
+
+
+// Call the functions to that the line below logs out 10
+console.log(myPoints);
+
+
