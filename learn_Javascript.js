b208797b30ae7a026b9b63df032691e5acd1454e
zhussai1405 <zhussai1405@gmail.com>
2024-09-23 11:36:35 +0500

    commit new changes

--- learn_Javascript.js
+++ learn_Javascript.js
@@ -36,4 +36,52 @@ removePoints()
 // Call the functions to that the line below logs out 10
 console.log(myPoints);
 
+let firstBatch = 5;
+let secondBatch = 7;
+let count = firstBatch + secondBatch;
+console.log(count);
+
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints +50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints -75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints +45;
+console.log(bonusPoints);
+
+// Create a function that logs out the sum of all the lap times
+let lap1 = 32;
+let lap2 = 33;
+let lap3 = 35;
+
+function logLapTime(){
+    let totalTime = lap1 + lap2 + lap3;
+    console.log(totalTime);
+}
+logLapTime();
+
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+let lapsCompleted = 0;
+
+function incrementLap(){
+    lapsCompleted = lapsCompleted +1;
+
+}
+incrementLap()
+incrementLap()
+incrementLap()
+
+console.log(lapsCompleted);
 
