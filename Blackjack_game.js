ca9c3bd8c11f00cc9adc3a45897bcb1fa71b9e2e
zhussai1405 <zhussai1405@gmail.com>
2024-10-15 19:12:16 +0500

    commit changes
    new changes in file

--- Blackjack_game.js
+++ Blackjack_game.js
@@ -3,8 +3,9 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6;
-let secondCard = 9;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false
 let isAlive = true
@@ -12,19 +13,37 @@ let message = ""
 
 let messageEl = document.getElementById("message-el")
 //let sumEl = document.getElementById("sum-el")
-let sumEl = document.querySelector("#sum-el") 
+let sumEl = document.getElementById("#sum-el") 
+let cardsEl = document.getElementById("card-el")
 
+function getRandomCard(){
+    return Math.floor(Math.random()* 13)+ 1
+}
 function startGame(){
+    renderGame()
+}
+function renderGame(){
+    cardsEl.textContent = "Cards: "
+    for(let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
 if(sum <=20 ){
     message = "Do you want to draw a new card?"
 } else if (sum === 21) {
-    message = "wohoo! you got Blackjack!"
+    message = "you got Blackjack!"
     hasBlackJack = true
-} else 
-{
+} else {
     message = "Yor're out of the game!"
     isAlive = false
 }
 messageEl.textContent = message
 }
+
+function newCard(){
+    let card = getRandomCard();
+    sum += card
+    console.log("cards")
+     renderGame()
+}
+
