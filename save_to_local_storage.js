beba6325ca7517be71353559535a286356712ee8
zhussai1405 <zhussai1405@gmail.com>
2024-10-25 18:52:53 +0500

    init commit
    exercises

---
+++ save_to_local_storage.js
@@ -0,0 +1,6 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+let myCredits = localStorage.getItem("myCredits")
+console.log(myCredits)
